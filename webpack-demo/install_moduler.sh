d2a11ebbbfccd43052557a40567d05c848e20315
yangyonghui <yangyonghui@meituan.com>
2018-05-31 18:01:09 +0800

    small change

--- webpack-demo/install_moduler.sh
+++ webpack-demo/install_moduler.sh
@@ -1,1 +1,2 @@
 cnpm install --save-dev css-selector-tokenizer lodash style-loader css-loader file-loader
+cnpm install --save-dev webpack-dev-server
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,9 +2,13 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     output: {
 	filename: 'main.js',
 	path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+	contentBase: './dist'
     },
     module: {
 	rules: [
@@ -13,6 +17,12 @@ module.exports = {
 		use: [
 		    'style-loader',
 		    'css-loader'
+		]
+	    },
+	    {
+		test: /\.(png|svg|jpg|gif)$/,
+		use: [
+		    'file-loader'
 		]
 	    }
 	]
